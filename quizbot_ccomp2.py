ea7b4d1e8a631c4e4dbc5f3b548ddf45d586f233
raja <patrabiswajit133@gmail.com>
2019-03-05 10:33:49 +0530

    with added control over computers

--- quizbot_ccomp2.py
+++ quizbot_ccomp2.py
@@ -7,32 +7,27 @@ port=12345
 #lock=threading.Lock()
 question=""
 answers=['','','']
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((ip,port))
 while True:
- try:   
-  while True:
-   try:   
-    
-    print(">>>>>>>successfully connecte to server")
-    break
-   except:
-       print(">>>>>>Trying again")
-  ind=s.recv(1024)
-  ind=ind.decode(encoding='utf-8')
-  ind=ind.split("%^%")
-  question=ind[0]
-  for x in range(3):
-      answers[x]=ind[x+1]
-  print("Got a question from main server\n"+question,answers[0],answers[1],answers[2])
-  s.sendall("1".encode())
-  s.sendall("2".encode())
-  
+   s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   s.connect((ip,port))
+   while True:
+          try:   
+                 s.sendall("2".encode())
+                 ind=s.recv(1024)
+                 ind=ind.decode(encoding='utf-8')
+                 ind=ind.split("5662")
+                 question=ind[0]
+                 for x in range(3):
+                     answers[x]=ind[x+1]
+                 print("Got a question from main server\n"+question,answers[0],answers[1],answers[2])
+                 s.sendall("1".encode())
+                 s.sendall("2".encode())
+                 
   
     
- except Exception as e:
-     print(">>>>>>connection retrying new QUESTION "+str(e))
+          except Exception as e:
+              print(">>>>>>connection retrying new QUESTION "+str(e))
+   s.close()
         
         
         
--- quizbot_mcomp.py
+++ quizbot_mcomp.py
@@ -6,8 +6,8 @@ import numpy as np
 
 lock=threading.Lock()
 
-question=""
-answers=['','','']
+#question=""
+#answers=['','','']
 data=''
 points=[0,0,0]
 qc=[0,0,0]
@@ -17,7 +17,9 @@ def plotshow():
     global mpoints
     global points
     while True:
-        plt.bar(x=[0,2,4],height=points)
+        color=['blue','blue','blue']
+        color[points.index(max(points))]='green'
+        plt.bar(x=[0,2,4],height=points,color=color)
         plt.draw()
         plt.pause(1)
         plt.clf()
@@ -40,17 +42,14 @@ def sortingcomputer(conn,mode):
     global answers
     global data
     global qc
+    global points
     print("mode="+mode)
     if mode=='M':
         print("Successfully connected with MOBILE :",conn)
         data=conn.recv(1024).decode(encoding='utf-8')
         print("data from mobile:"+data)
-        dat=data.split("5662")
-        question=dat[0]
-        for x in range(3):
-            answers[x]=dat[x+1]
-        qc=[1,1,1]
-        print("got question",question,answers[0],answers[1],answers[2])    
+        with lock:
+            qc=[1,1,1]          
         conn.close()
     elif mode=="2":
         print("Successfully connected to 2nd computer :",conn)
@@ -59,26 +58,25 @@ def sortingcomputer(conn,mode):
                     pass
             with lock:
                     qc[0]=0
+                    points=[0,0,0]
             conn.sendall(data.encode())
-            points=[0,0,0]
             print("sent question to 2nd computer")
             while True:
               inpd=conn.recv(1).decode(encoding='utf-8')
               if inpd=='Q':
                 print("2nd computer work completed")
-                break
+                conn.close()
+                return
               else:
                    print("2nd computer ->",inpd)
                    with lock:
                         points[int(inpd)]+=1
                    if(qc[0]==1):
-                       conn.sendall('!'.encode())
-                       break
-                   else:
-                       conn.sendall('#'.encode())
+                       conn.close()
+                       return
                    
                 
-#threading.Thread(target=plotshow).start()
+threading.Thread(target=plotshow,name='plotthread').start()
 server = ThreadedTCPServer(('', 12345), ThreadedTCPRequestHandler)
 with server:
     print("plot done and server started")
