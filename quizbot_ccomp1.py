a87d717d8ef8bc7132636ad244d6fc2f5292f013
raja <patrabiswajit133@gmail.com>
2019-03-05 12:59:34 +0530

    Added results display

--- quizbot_ccomp1.py
+++ quizbot_ccomp1.py
@@ -1,198 +1,56 @@
-import colorama
-import re
-import os
-import time
-import crayons
-from nltk.corpus import stopwords
-import pytesseract
+import socket
+#from threading import Thread
 import requests
-import pyautogui
-import json as m_json
 from bs4 import BeautifulSoup
-from PIL import Image, ImageEnhance
-import argparse
-colorama.init()
-major=[0,0,0,0]
+import re
+#TODO:define ip of the main comp
+ip='127.0.0.1'
+port=12345
 
-mystopword={".","(",")"}
-stopwords={"which","where","when","is","does","these","the"}
+def parse_result(html,soc):
+   soup=BeautifulSoup(html,'html.parser')
+   
+question=""
+answers=['','','']
+multi=[0,0,0]
+while True:
+   s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   s.connect((ip,port))
+   question=""
+   answers=['','','']
+   multi=[0,0,0]
+   try:
+           s.sendall("1".encode())
+           ind=s.recv(1024)
+           ind=ind.decode(encoding='utf-8')
+           ind=ind.split("5662")
+           question=ind[0]
+           for x in range(3):
+               answers[x]=ind[x+1]
+           print("Got a question from main server\n"+question,answers[0],answers[1],answers[2])
+           search = " "+question +" \""+ answers[0]+"\""
+           print(search)
+           r = requests.get("https://www.google.com/search", params={'q':search})
+           print(answers[0])
+           
+           s.sendall("R".encode())
+           soup=BeautifulSoup(r.text,'html.parser')
+           res=soup.find("div", {"id": "topstuff"})
+           if "No results found for" in res.get_text():
+              s.sendall("-".encode())
+              print("No results found......")
+           else:   
+              res = soup.find("div", {"id": "resultStats"})
+              print(res)
+              s.sendall(res.get_text().encode())
+                    
+           parse_result(r.texr,s)
+           s.sendall("Q".encode())
+   except Exception as e:
+        print(">>>>>>connection retrying new QUESTION "+str(e))
+   s.close()
 
-def findregex(resultin):
-    resultin=resultin.split()
-    for xr in resultin:
-        if re.search(xr,answers[0], re.IGNORECASE):
-            gotit[0]+=1
-        elif re.search(xr,answers[1], re.IGNORECASE):
-            gotit[1]+=1   
-        elif re.search(xr,answers[2], re.IGNORECASE):
-            gotit[2]+=1
-def parse_results(html, keyword,ans):
-    soup = BeautifulSoup(html, 'html.parser')
-    res = soup.find("div", {"id": "resultStats"})
-    print(res)
-    found_results = []
-    rank = 1
-    result_block = soup.find_all('div', attrs={'class': 'g'})
-    print("finding")
-    for result in result_block:
-
-        link = result.find('a', href=True)
-        
-        title = result.find('h3', attrs={'class': 'r'})
-        description = result.find('span', attrs={'class': 'st'})
-        #data=[b.string for b in soup.findall('b')]
-        #print(data)
-        if link and title and description:
-            link = link['href']
-            title = title.get_text()
-            #print(description)
-            descriptions = description.get_text()
-            if link != '#' and rank < 3:
-                #found_results.append({'keyword': keyword,'rank': rank, '\ntitle': title, '\ndescription': description})
-                #found_results.append(descriptions)
-                found_results.append(" ".join(filter(lambda w: not w in stopwords,descriptions.split())))
-                print(crayons.white(title))
-                data=[b.string for b in description.find_all('b') if b.string!="..."]
-                major[ans]+=len(data)
-                print(crayons.red(data,bold=True)) 
-                rank += 1
-    return found_results
 
 
-question = "Which of these is known as sleeping policeman of roads"#"The guinea pig is native to which continent?"
-answersen = ["Traffic lights", "Speed Bumps", "Zebra crossing"]
-answers=[]
 
-question=question.replace("?","")
-question=question.lower()
-for a in answersen:
-    xtemp=a.replace("." ," ")
-    xtemp=xtemp.lower()
-    answers+=[xtemp]
-question=question.replace("Which","")
-question=question.replace("What","") 
-for i, a in enumerate(answers):
-    answers[i] = a.replace("?", "ti")
 
-print("\n\n{}\n\n{}\n\n".format(
-    crayons.green(question),
-    crayons.red(", ".join(answers))
-))
-question=" ".join(filter(lambda w: not w in stopwords,question.split()))
-#question = question.split(' ', 1)[1]
-#question = question[:-1] 
-#question = question.replace(" ", "\" \"")
-print("\n\n{}\n\n{}\n\n".format(
-    crayons.green(question),
-    crayons.red(", ".join(answers))
-))
-maxi=[0,0,0]
-'''
-query = urllib.urlencode ( { 'q' : search } )
-response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
-json = m_json.loads ( response )
-results = json [ 'responseData' ] [ 'results' ]
-for result in results:
-  title = result['title']
-  url = result['url']   # was URL in the original and that threw a name error exception
-  print ( title + '; ' + url )
-'''  
-gotit=[0,0,0]
-search = ""+question +" (("+ answers[1] +") | ("+answers[0]+") | ("+answers[2]+"))"
-print(search)
-r = requests.get("https://www.google.com/search", params={'q':search})
-results = parse_results(r.text,search,3)
-for result in results:
-    '''
-     if re.match("("+answers[0]+")"+"("+answers[1]+")"+"("+answers[2]+")", result, re.IGNORECASE) :
-       gotit[1]+=1
-     if re.search(answers[2], result, re.IGNORECASE):
-       gotit[2]+=1
-     if re.search(answers[0], result, re.IGNORECASE):
-       gotit[0]+=1'''
-    result=re.sub("\.|\)|\(","",result)
-    findregex(result)
-    print(crayons.green(result))
-'''if gotit[0]==gotit[1]==gotit[2]:
- print(crayons.magenta("INValid"))
-else: 
- mn,idx = max( (gotit[i],i) for i in range(3) )
- print(crayons.magenta("answer is in Interference APPROACH : "+answers[idx],bold=True)) 
- '''    
-search = " "+question +" \""+ answers[0]+"\""#+" -\""+answers[1]+"\" -\""+answers[2]+"\""
-print(search)      
-r = requests.get("https://www.google.com/search", params={'q':search})
-#soup = BeautifulSoup(r.text, "html.parser")
-#res = soup.find("div", {"id": "resultStats"})
-print (answers[0])
-results = parse_results(r.text,search,0)
-for result in results:
-     if answers[1] in result:
-       maxi[1]+=1
-     if answers[2] in result:
-       maxi[2]+=1  
-     print(crayons.cyan(result))  
-#name_box = soup.find('div', attrs={'id': 'topstuff'})
-#print(name_box)
-#print(res)    
-#hmax[0]=int(res.text.replace(",", "").split()[1])
-#print (hmax[0])
-
-search = ""+question +" \""+ answers[1]+"\""#+" -\""+answers[0]+"\" -\""+answers[2]+"\""
-print(search)
-r = requests.get("https://www.google.com/search", params={'q':search})
-#soup = BeautifulSoup(r.text, "html.parser")
-#res = soup.find("div", {"id": "resultStats"})
-print (answers[1])
-results = parse_results(r.text,search,1)
-for result in results:
-     if answers[0] in result:
-       maxi[0]+=1
-     if answers[2] in result:
-       maxi[2]+=1
-     print(crayons.yellow(result))
-#name_box = soup.find('div', attrs={'id': 'topstuff'})
-#print(name_box)
-#print(res)
-#hmax[1]=int(res.text.replace(",", "").split()[1])
-#print (hmax[1])
-
-search = ""+question +" \""+ answers[2] +"\""#+" -\""+answers[0]+"\" -\""+answers[1]+"\""
-print(search)
-r = requests.get("https://www.google.com/search", params={'q':search})
-#print(r)
-#soup = BeautifulSoup(r.text, "html.parser")
-#res = soup.find("div", attrs ={"id": "resultStats"})
-print(answers[2])
-results = parse_results(r.text,search,2)
-for result in results:
-     if answers[0] in result:
-       maxi[0]+=1
-     if answers[1] in result:
-       maxi[1]+=1
-     print(crayons.green(result))
-#print(res)
-#name_box = soup.find('div', attrs={'id': 'topstuff'})
-#print(name_box) 
-#hmax[2]=int(res.text.replace(",", "").split()[1])
-#print (hmax[2])
-print(maxi)
-#mn,idx = max( (hmax[i],i) for i in range(len(hmax)) )
-#print(crayons.green(answers[idx]))
-major[3]=0
-mn,idx = max( (major[i],i) for i in range(4) )
-print(crayons.magenta("answer is in READ APPROACH : "+answers[idx],bold=True))
-if maxi[0]==maxi[1]==maxi[2]:
- print(crayons.magenta("INValid"))
-else: 
- mn,idx = max( (maxi[i],i) for i in range(3) )
- print(crayons.magenta("answer is in Interference APPROACH : "+answers[idx],bold=True))
-if gotit[0]==gotit[1]==gotit[2]:
- print(crayons.magenta("INValid"))
-else: 
- mn,idx = max( (gotit[i],i) for i in range(3) )
- print(crayons.magenta("answer is in normal APPROACH : "+answers[idx],bold=True))
-print("gotit",gotit)
-input("Continue?")
-time.sleep(0.1)
-os.system("cls")
--- quizbot_mcomp.py
+++ quizbot_mcomp.py
@@ -6,20 +6,20 @@ import numpy as np
 
 lock=threading.Lock()
 
-#question=""
-#answers=['','','']
 data=''
 points=[0,0,0]
-qc=[0,0,0]
+qc=[0,0,0,0]
+label=['','','']
 plt.ion()
 
 def plotshow():
     global mpoints
     global points
+    global label
     while True:
         color=['blue','blue','blue']
         color[points.index(max(points))]='green'
-        plt.bar(x=[0,2,4],height=points,color=color)
+        plt.bar(x=[0,2,4],height=points,color=color,tick_label=label)
         plt.draw()
         plt.pause(1)
         plt.clf()
@@ -49,31 +49,39 @@ def sortingcomputer(conn,mode):
         data=conn.recv(1024).decode(encoding='utf-8')
         print("data from mobile:"+data)
         with lock:
-            qc=[1,1,1]          
+            qc=[1,1,1,1]
+            points=[0,0,0]
         conn.close()
-    elif mode=="2":
-        print("Successfully connected to 2nd computer :",conn)
-        while True:
-            while(qc[0]==0):
+    elif mode=="0" or mode=='1' or mode=='2' or mode=='3':
+            print("Successfully connected to ["+mode+"] computer :",conn)
+            while(qc[int(mode)]==0):
                     pass
             with lock:
-                    qc[0]=0
-                    points=[0,0,0]
+                    qc[int(mode)]=0
             conn.sendall(data.encode())
-            print("sent question to 2nd computer")
+            print("sent question to ["+mode+"] computer")
             while True:
-              inpd=conn.recv(1).decode(encoding='utf-8')
+              inpd=conn.recv(1).decode()
               if inpd=='Q':
-                print("2nd computer work completed")
+                print("["+mode+"] computer work completed")
                 conn.close()
                 return
+              elif inpd=="R":
+                  inpfc=conn.recv(1024).decode()
+                  with lock:
+                     if(inpfc=='-'):
+                       points[int(mode)-1]=-10
+                     label[int(mode)-1]=inpfc
               else:
-                   print("2nd computer ->",inpd)
-                   with lock:
-                        points[int(inpd)]+=1
-                   if(qc[0]==1):
+                   print("["+mode+"] computer ->",inpd)
+                   if(qc[int(mode)]==1):
                        conn.close()
                        return
+                   with lock:
+                       if(qc[int(mode)]==1):
+                          conn.close()
+                          return
+                       points[int(inpd)]+=1
                    
                 
 threading.Thread(target=plotshow,name='plotthread').start()
